17515b7d490d89dc431f8f8eb743611e9b468969
bcyes <bcyes168@hotmail.com>
2019-12-30 04:26:25 +0800

    提交springboot学习demo-girl

---
+++ src/main/java/com/example/controller/UserController.java
@@ -0,0 +1,62 @@
+package com.example.controller;
+
+import com.example.entity.User;
+import com.example.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    UserRepository userRepository;
+
+
+    @GetMapping(value = "/userList")
+
+    public List<User> userList(){
+        List userList = userRepository.findAll();
+        return userList;
+    }
+
+    @GetMapping("/user/{id}")
+    public Optional<User> getUser(@PathVariable("id")Integer id){
+        Optional<User> user = userRepository.findById(id);
+        return user;
+    }
+
+    @PostMapping("/user")
+    public User insertUser(User user){
+        user.setBirthday(new Date());
+        user.setCreatdTime(new Date());
+        User save = userRepository.save(user);
+        return save;
+    }
+
+    @PatchMapping("/patch")
+    public User patchtUser(User user){
+        user.setBirthday(new Date());
+        user.setCreatdTime(new Date());
+        User save = userRepository.save(user);
+        return save;
+    }
+
+    @PutMapping("/put")
+    public User putUser(User user){
+        user.setBirthday(new Date());
+        user.setCreatdTime(new Date());
+        User save = userRepository.save(user);
+        return save;
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String deleteUser(@PathVariable("id")Integer id){
+        userRepository.deleteById(id);
+        return "已经删除"+id;
+    }
+
+}
