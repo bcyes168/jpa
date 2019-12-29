32bcb1416e3a16b821c958dec88c6e50b3d73bf2
bcyes <bcyes168@hotmail.com>
2019-12-30 04:39:55 +0800

    提交springboot学习demo-girl

---
+++ src/main/java/com/example/entity/User.java
@@ -0,0 +1,84 @@
+package com.example.entity;
+
+import javax.persistence.*;
+import java.util.Date;
+
+
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String email;
+    private String address;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date creatdTime;
+    @Temporal(TemporalType.DATE)
+    private Date birthday;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", address='" + address + '\'' +
+                ", creatdTime=" + creatdTime +
+                ", birthday=" + birthday +
+                '}';
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Date getCreatdTime() {
+        return creatdTime;
+    }
+
+    public void setCreatdTime(Date creatdTime) {
+        this.creatdTime = creatdTime;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public User() {
+    }
+}
